93a7519825a905b6f5f9c8b0d63c0c97181a71f1
maratgareev97 <maratgareev97@gmail.com>
2022-09-23 11:52:30 +0300

    config clear all

--- src/main/java/ru/springmavc/crud/SpringBootSecurityDemoApplication.java
+++ src/main/java/ru/springmavc/crud/SpringBootSecurityDemoApplication.java
@@ -1,12 +1,21 @@
 package ru.springmavc.crud;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
 
 @SpringBootApplication
+@PropertySource(value = {"classpath:db.properties", "classpath:hibernate.properties"})
+@EnableTransactionManagement(proxyTargetClass = true)
 public class SpringBootSecurityDemoApplication {
 
     public static void main(String[] args) {
@@ -17,6 +26,27 @@ public class SpringBootSecurityDemoApplication {
     @Bean
     HiddenHttpMethodFilter hiddenHttpMethodFilter() {
         return new HiddenHttpMethodFilter();
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("/WEB-INF/views/");
+        templateResolver.setSuffix(".html");
+        return templateResolver;
+    }
+
+    @Resource
+    private Environment env;
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName(env.getRequiredProperty("db.driver"));
+        ds.setUrl(env.getRequiredProperty("db.url"));
+        ds.setUsername(env.getRequiredProperty("db.username"));
+        ds.setPassword(env.getRequiredProperty("db.password"));
+        return ds;
     }
 
 
